bd6b3b59b2d3f2f3e9c396651efdc32923caf9f6
chinjja <chinjja@gmail.com>
2023-04-19 23:05:45 +0900

    flutter_chat_ui

--- lib/pages/chats/view/chat_page.dart
+++ lib/pages/chats/view/chat_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:portfolio/app/app.dart';
-import 'package:portfolio/models/models.dart';
 import 'package:portfolio/pages/pages.dart';
-import 'package:portfolio/services/services.dart';
 import 'package:portfolio/widgets/widgets.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart' as cu;
 
 class ChatPage extends HookConsumerWidget {
   final String chatId;
@@ -38,80 +36,30 @@ class ChatView extends HookConsumerWidget {
     }
     final provider = sendMessageProvider(chatId);
     final messages = ref.watch(getMessagesByChatIdProvider(chatId));
-    final controller = useTextEditingController();
     ref.listen(provider, (previous, next) {
       next.whenOrNull(
-        data: (data) {
-          controller.clear();
-        },
         error: (error, stackTrace) {
           context.showSnackbarOk(error.toString());
         },
       );
     });
-    final loading = ref.watch(provider).isLoading;
-    final enable = useState(false);
     return messages.when(
       loading: () => const LoadingView(),
       error: (error, stackTrace) => Text(error.toString()),
-      data: (data) => SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: data.isEmpty
-                  ? const Text('No Message')
-                  : ListView.builder(
-                      reverse: true,
-                      itemCount: data.length,
-                      itemBuilder: (context, index) {
-                        final message = data[index];
-                        return _MessageTile(
-                          key: ValueKey(message.id),
-                          message: message,
-                        );
-                      },
-                    ),
-            ),
-            const Divider(height: 0),
-            TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                hintText: '메시지를 입력하세요.',
-                suffix: IconButton(
-                  onPressed: loading || !enable.value
-                      ? null
-                      : () {
-                          ref.read(provider.notifier).send(controller.text);
-                        },
-                  icon: loading
-                      ? const CircularProgressIndicator()
-                      : const Icon(Icons.send),
-                ),
-              ),
-              onChanged: (value) {
-                enable.value = value.isNotEmpty;
-              },
-              onEditingComplete: loading || !enable.value
-                  ? null
-                  : () {
-                      ref.read(provider.notifier).send(controller.text);
-                    },
-            ),
-          ],
-        ),
+      data: (data) => cu.Chat(
+        messages: data
+            .map((e) => types.TextMessage(
+                  id: e.id,
+                  author: types.User(id: e.uid),
+                  text: e.message,
+                  createdAt: e.date?.millisecondsSinceEpoch,
+                ))
+            .toList(),
+        onSendPressed: (text) {
+          ref.read(provider.notifier).send(text.text);
+        },
+        user: types.User(id: user.uid),
       ),
-    );
-  }
-}
-
-class _MessageTile extends HookConsumerWidget {
-  final ChatMessage message;
-  const _MessageTile({super.key, required this.message});
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return ListTile(
-      title: Text(message.message),
-      subtitle: Text(message.uid),
     );
   }
 }
